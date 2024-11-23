624eb5df11e82c8cd51b8498d9f04df5deecee5f
gallianoms <ggallianoms@gmail.com>
2024-11-23 15:18:42 +0100

    chore: add firebase config to environments

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebaseConfig: {
+    apiKey: 'AIzaSyC3ZrFEbUMCTxTX3pCZIFRftWfh_Fj_Fj4',
+    authDomain: 'pppeople-29e8b.firebaseapp.com',
+    projectId: 'pppeople-29e8b',
+    storageBucket: 'pppeople-29e8b.firebasestorage.app',
+    messagingSenderId: '175296705927',
+    appId: '1:175296705927:web:b9f1ad7a339e03a543cf22',
+  },
+};
