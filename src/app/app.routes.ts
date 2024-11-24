2e338bb77b7af899959ac512f1e98944323877d4
gallianoms <ggallianoms@gmail.com>
2024-11-24 01:28:30 +0100

    feat: add routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'welcome'
+  },
+  {
+    path: 'welcome',
+    loadComponent: () => import('./features/welcome/welcome.component').then(c => c.WelcomeComponent)
+  },
+  {
+    path: 'room/:id',
+    loadComponent: () => import('./features/room/room.component').then(c => c.RoomComponent)
+  }
+];
