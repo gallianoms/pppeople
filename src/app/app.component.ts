248a5903b67b829b7e4597d476ff478596306f0f
gallianoms <ggallianoms@gmail.com>
2025-01-07 20:13:33 +0100

    chore: update Angular dependencies and add @vercel/analytics integration for enhanced analytics tracking in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { inject } from '@vercel/analytics';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +9,9 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'pppeople';
+  title = 'pppeople | Planning Poker for teams';
+
+  constructor() {
+    inject();
+  }
 }
